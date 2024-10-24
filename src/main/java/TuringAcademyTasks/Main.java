2e12dd424e47287e78059ace73dea076b648e189
revanaghayev <rvanagayev17@gmail.com>
2024-10-24 20:33:54 +0400

    Print in Java

--- src/main/java/TuringAcademyTasks/Main.java
+++ src/main/java/TuringAcademyTasks/Main.java
@@ -2,6 +2,6 @@ package TuringAcademyTasks;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("1\n2\n3\n4\n5\n");
     }
 }
