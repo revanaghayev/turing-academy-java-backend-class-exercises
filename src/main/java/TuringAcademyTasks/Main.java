4a1100203afca95213d54cf4a20290f2ff39c4cb
revanaghayev <rvanagayev17@gmail.com>
2024-10-26 15:15:41 +0400

    Printing different data types in Java

--- src/main/java/TuringAcademyTasks/Main.java
+++ src/main/java/TuringAcademyTasks/Main.java
@@ -2,6 +2,19 @@ package TuringAcademyTasks;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1\n2\n3\n4\n5\n");
+        byte a = 1;
+        short b = 2;
+        int c = 3;
+        long d = 4;
+        float e = 3.14159265f;
+        double f = 333;
+        char symbol = 'C';
+        boolean isFlag = true;
+
+        System.out.print(a+"\n");
+        System.out.println(b+"\n"+c+"\n"+d+"\n"+f+"\n");
+        System.out.printf("If we round pi to the decimals it will be %.0f \n", e);
+        System.out.printf("Java is from %c language family. \n", symbol);
+        System.out.printf("Is this %b or not?", isFlag);
     }
 }
