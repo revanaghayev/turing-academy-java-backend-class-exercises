67f220e1bd5ad1219f18831923947ca1e46d7101
revanaghayev <rvanagayev17@gmail.com>
2024-10-26 17:58:47 +0400

    This is operators

---
+++ src/main/java/TuringAcademyTasks/Operators.java
@@ -0,0 +1,23 @@
+package TuringAcademyTasks;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int number1 = 15;
+        int number2 = 7;
+
+        int add = number1 + number2;
+        int sub = number1 - number2;
+        int mult = number1 * number2;
+        int div = number1 / number2;
+        int mod = number1 % number2;
+
+
+        System.out.println("Addition is: "+add);
+        System.out.println("Substraction is: "+sub);
+        System.out.println("Multiplication is: "+mult);
+        System.out.println("Division is: "+div);
+        System.out.println("Modulus is: "+mod);
+    }
+}
