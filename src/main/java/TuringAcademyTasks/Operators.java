cf16f7cde58d169e2ba7d06ce642a4e196c1648f
revanaghayev <rvanagayev17@gmail.com>
2024-10-26 18:47:28 +0400

    Compare number if it is even or odd

--- src/main/java/TuringAcademyTasks/Operators.java
+++ src/main/java/TuringAcademyTasks/Operators.java
@@ -1,23 +1,31 @@
 package TuringAcademyTasks;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Operators {
 
     public static void main(String[] args) {
 
-        int number1 = 15;
-        int number2 = 7;
-
-        int add = number1 + number2;
-        int sub = number1 - number2;
-        int mult = number1 * number2;
-        int div = number1 / number2;
-        int mod = number1 % number2;
-
-
-        System.out.println("Addition is: "+add);
-        System.out.println("Substraction is: "+sub);
-        System.out.println("Multiplication is: "+mult);
-        System.out.println("Division is: "+div);
-        System.out.println("Modulus is: "+mod);
+        System.out.println("Enter the number: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        boolean odd = true;
+        System.out.println(number+ (odd ? " is odd." : " is even."));
+//        int number1 = 15;
+//        int number2 = 7;
+//
+//        int add = number1 + number2;
+//        int sub = number1 - number2;
+//        int mult = number1 * number2;
+//        int div = number1 / number2;
+//        int mod = number1 % number2;
+//
+//        System.out.println("Addition is: "+add);
+//        System.out.println("Substraction is: "+sub);
+//        System.out.println("Multiplication is: "+mult);
+//        System.out.println("Division is: "+div);
+//        System.out.println("Modulus is: "+mod);
     }
 }
