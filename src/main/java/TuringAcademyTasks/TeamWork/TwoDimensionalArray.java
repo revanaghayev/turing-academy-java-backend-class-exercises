40a3807fa66289db4d6f494a6b967b0e28a9c598
revanaghayev <rvanagayev17@gmail.com>
2024-11-11 19:44:53 +0400

    Team work task: Two Dimensional Array

--- src/main/java/TuringAcademyTasks/TeamWork/TwoDimensionalArray.java
+++ src/main/java/TuringAcademyTasks/TeamWork/TwoDimensionalArray.java
@@ -1,4 +1,32 @@
 package TuringAcademyTasks.TeamWork;
 
+import java.util.Scanner;
+
 public class TwoDimensionalArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num = sc.nextInt();
+        int[][] arr = new int[num][];
+        int temp;
+        int i;
+        int j;
+        for (i = 0; i < num; i++) {
+            arr[i] = new int[i+1];
+            temp = i+1;
+            for (j = 0; j < i+1; j++) {
+                arr[i][j] = temp;
+            }
+        }
+
+        for (i = 0; i < num; i++) {
+            for (j = 0; j < i+1; j++) {
+                System.out.print(arr[i][j] +" ");
+            }
+            System.out.println();
+        }
+
+    }
 }
